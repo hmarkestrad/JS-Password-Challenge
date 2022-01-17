b8fe8263fb63197500bb0c35e98442920043816e
Mark Estrada <h.mark.estrada@gmail.com>
2022-01-17 03:29:55 -0600

    Javascript Update

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,7 @@
 // Get the modal
 var modal = document.getElementById("myModal");
+var modalmessageAlert = document.getElementById("myModalMessageRED");
+var modalmessage = document.getElementById("myModalMessage");
 var btn = document.getElementById("FeatureSelection");
 var CloseModal = document.getElementsByClassName("close")[0];
 var next = document.getElementById("mybutton");
@@ -7,11 +9,10 @@ var step_02 = document.getElementById("step_02");
 var step_03 = document.getElementById("step_03");
 var step_04 = document.getElementById("step_04");
 var step_05 = document.getElementById("step_05");
-var step_06 = document.getElementById("step_06");
+//var step_06 = document.getElementById("step_06");
 var next = document.getElementById("next");
 var FeatureSelection = document.getElementById("FeatureSelection");
 var SelectionReset = document.getElementById("SelectionReset");
-//var password = document.getElementById('password');
 var submit = document.getElementById("submit");
 let step = 0;
 
@@ -45,9 +46,6 @@ next.onclick = function(event) {
   }
   if (step >= 4) { 
     step_05.style.display = "block";
-  }
-  if (step >= 5) { 
-    step_06.style.display = "block";
     submit.style.display = "block";
     next.style.display = "none";
   }
@@ -56,29 +54,46 @@ next.onclick = function(event) {
 submit.onclick = function(event) {
 
   var numbers = document.getElementById("numbers").checked;
-  document.getElementById("demo").innerHTML = x;
+  var symbols = document.getElementById("symbols").checked;
+  var upper = document.getElementById("upper").checked;
+  var lower = document.getElementById("lower").checked;
+
+  //validate
+  if(numbers == true || symbols == true || upper == true || lower == true ) {
+  
+  var ddlValue = document.getElementById("step_01");
+  var pwlength = ddlValue.value;
+  console.log(pwlength);
   console.log(numbers);
-
-
+  console.log(symbols);
+  console.log(upper);
+  console.log(lower);
 
   modal.style.display = "none";
   FeatureSelection.style.display = "none";
   SelectionReset.style.display = "inline";
   document.getElementById('password').removeAttribute('placeholder');
+  
   function password_generator( len ) {
-    var length = (len)?(len):(128);
-    var string = "abcdefghijklmnopqrstuvwxyz"; //to upper 
-    var numeric = '0123456789';
+    var length = (len)?(len):(pwlength);
+
+    var string = " ";
+    var numeric = "0123456789";
     var punctuation = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
+
+    if(numbers == false) { var numeric = " "; }
+    if(lower == true) { string = "abcdefghijklmnopqrstuvwxyz"; } 
+    if(upper == true) { string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; } 
+    if(upper == true && lower == true) { string = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz"; } 
+    if(symbols == false) { punctuation = " "; }
+
     var password = "";
     var character = "";
-    var crunch = true;
     while( password.length<length ) {
         entity1 = Math.ceil(string.length * Math.random()*Math.random());
         entity2 = Math.ceil(numeric.length * Math.random()*Math.random());
         entity3 = Math.ceil(punctuation.length * Math.random()*Math.random());
         hold = string.charAt( entity1 );
-        hold = (password.length%2==0)?(hold.toUpperCase()):(hold);
         character += hold;
         character += numeric.charAt( entity2 );
         character += punctuation.charAt( entity3 );
@@ -89,6 +104,11 @@ submit.onclick = function(event) {
   }
 
   document.getElementById("password").innerHTML = password_generator();
+  }
+
+  else { 
+    modalmessageAlert.style.display = "block";
+    modalmessage.style.display = "none";
+  }
 
 }
-
